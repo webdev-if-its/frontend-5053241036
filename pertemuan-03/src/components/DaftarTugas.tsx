068fa32536cb314b22d77c99ba407490a288ac22
zahrafidela <zahrafi0310@gmail.com>
2026-09-16 17:10:20 +0700

    pertemuan-03: level 2 selesai

--- pertemuan-03/src/components/DaftarTugas.tsx
+++ pertemuan-03/src/components/DaftarTugas.tsx
@@ -3,6 +3,19 @@
 // elemen array (manfaatkan kembali komponen ItemTugas dari Level 2a, jangan
 // tulis ulang markup-nya). key ditaruh di <ItemTugas key={...} />, bukan di
 // dalam ItemTugas itu sendiri. Lihat SOAL.md untuk kontrak lengkap.
-export function DaftarTugas(props: any) {
-  return <p>TODO</p>
+import type { Tugas } from "../types"
+import { ItemTugas } from "./ItemTugas"
+
+type DaftarTugasProps = {
+  tugas : Tugas[]
+}
+
+export function DaftarTugas({tugas} : DaftarTugasProps) {
+  return (
+    <ul>
+      {tugas.map((t) => (
+        <ItemTugas key={t.id} tugas={t} />
+      ))}
+    </ul>
+  )
 }
