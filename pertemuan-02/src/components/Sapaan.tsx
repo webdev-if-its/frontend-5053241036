eb2645021491c021fd3c3bf77819202d3d2572b4
zahrafidela <zahrafi0310@gmail.com>
2026-09-16 13:20:44 +0700

    pertemuan-02: level 7 selesai

--- pertemuan-02/src/components/Sapaan.tsx
+++ pertemuan-02/src/components/Sapaan.tsx
@@ -2,6 +2,10 @@
 // OPSIONAL). Kalau nama tidak diberikan, gunakan nilai default "Tamu" —
 // cari tahu caranya lewat default parameter destructuring, bukan lewat
 // if/else manual. Render <p>Halo, {nama}!</p>. Lihat SOAL.md.
-export function Sapaan(props: any) {
-  return <p>TODO</p>
+
+type SapaanProps = {
+  nama? : string
+}
+export function Sapaan({nama = 'Tamu'} : SapaanProps) {
+  return <p>Halo, {nama}!</p>
 }
